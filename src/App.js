ec4960c660a3f1323cae821947d00fecd60e8c70
coder9s <luandevpro@gmail.com>
2018-03-22 10:59:33 +0700

    bootstrap 4 font-awesome jquery 3-3-1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Product from './components/Product';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Product />
+      </div>
+    );
+  }
+}
+
+export default App;
